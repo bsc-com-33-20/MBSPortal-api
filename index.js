ee19dfc9beed1bb1e5cfc062b996559fdc17c869
bsc-com-33-20 <bsc-com-33-20@unima.ac.mw>
2023-04-18 08:59:43 +0200

    patch name using patch request

--- index.js
+++ index.js
@@ -11,6 +11,41 @@ const dbConnection = require('./src/utils/mysql.connector')
 const { Post } = require('./src/posts/post.model')
 //const post = require('./src/posts/post.model')
 
+//Update existing post
+
+app.patch('/api/v1/posts/:id', function(req, res) {
+
+    //get id, select post from db, update the  post of it exists
+    //console.log(req.params)
+    const sql2 = `SELECT * FROM posts WHERE id=${req.params.id} LIMIT 1`
+    
+    return dbConnection.query(sql2, function(err, rows){
+        if (err) throw err
+
+        const post = req.body
+        console.log(post)
+
+        if (rows.length >= 1){
+            
+            const updateSql = `UPDATE posts SET name='${post.name}',summary='${post.summary} WHERE id=${rows[0].id}`
+
+            console.log(updateSql)
+
+            return dbConnection.query(updateSql, function(err, result){
+                return res.json(result)
+            })
+        } else {
+            return res.status(404).json({
+                status: false,
+                statusCode: 404,
+                message: `post with id ${req.params.id} does not exist` 
+            })
+        }
+        //return res.json(rows)
+    })
+    
+})
+
 app.get('/api/v1', function(req, res){
     return res.json(req.headers)
 })
@@ -27,8 +62,8 @@ app.get('/api/v1/posts', function(req, res){
 
 app.post('/api/v1/posts', function(req, res){
     
-    const { name, imageurl, summery } = req.body // destructure sent properties from the REQUEST body
-    var sql = `INSERT INTO posts (name, imageurl, summery) VALUES ('${name}','${imageurl}','${summery}')`;
+    const { name, imageUrl, summary } = req.body // destructure sent properties from the REQUEST body
+    var sql = `INSERT INTO posts (name, imageurl, summary) VALUES ('${name}','${imageUrl}','${summary}')`;
 
     //var sql = "INSERT INTO posts (name,imageurl,summery) VALUES ('Bisiness Inc', 'https://business.com', 'abcdef ghijk' )";
         dbConnection.query(sql, function (err, result) {
